44c2855d85c8865e5b93c8b31ecd667badb458c7
nicoleal88 <nicoleal88@gmail.com>
2020-08-25 11:25:49 -0300

    fix2

--- public/sketch.js
+++ public/sketch.js
@@ -93,15 +93,16 @@ gui.setPosition(gui_x, gui_y);
 
 // set slider range for setpoints
 // sliderRange(spMin, spMax, 1);
-gui.addGlobals('setpoint1');
+// gui.addGlobals('setpoint1');
+gui.addGlobals('spMax');
 
 // sliderRange(spMin, spMax, 1);
-gui.addGlobals('setpoint2');
+// gui.addGlobals('setpoint2');
 
 // sliderRange(spMin, spMax, 1);
-gui.addGlobals('setpoint3');
+// gui.addGlobals('setpoint3');
 
-gui.addGlobals('label1', 'label2', 'label3',);
+// gui.addGlobals('label1', 'label2', 'label3',);
 
 // Don't loop automatically
 noLoop();
