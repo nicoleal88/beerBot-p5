67f4aac3da9f7bc1db0f51a76d8959e8204717d9
nicoleal88 <nicoleal88@gmail.com>
2020-08-25 10:40:50 -0300

    Python sends random data to AWS server

--- python_post_request.py
+++ python_post_request.py
@@ -1,20 +1,32 @@
 # importing the requests library 
 import requests 
 import json
+import random
+import time
 from datetime import datetime
-# endpoint = "http://ec2-13-58-79-243.us-east-2.compute.amazonaws.com:3001/data"
-endpoint = "http://localhost:3000/data"
+endpoint = "http://ec2-13-58-79-243.us-east-2.compute.amazonaws.com:3001/data"
+# endpoint = "http://localhost:3000/data"
 
-# current date and time
-now = datetime.now()
-timestamp = int(datetime.timestamp(now)*1000)
+t1 = 20
+t2 = 20
+t3 = 20
 
-payload = {'timestamp' : timestamp, 't1': 26.2, 't2': 18.4, 't3': 19.4}
+while True:
+    t1 += random.random()*2-1
+    t2 += random.random()*2-1
+    t3 += random.random()*2-1
+    # current date and time
+    now = datetime.now()
+    timestamp = int(datetime.timestamp(now)*1000)
 
-# sending post request and saving response as response object 
-r = requests.post(url = endpoint, json = payload) 
+    payload = {'timestamp' : timestamp, 't1': t1, 't2': t2, 't3': t3}
 
-# extracting response text 
-resp = r.text
-status = r.status_code 
-print(resp, status) 
+    # sending post request and saving response as response object 
+    r = requests.post(url = endpoint, json = payload) 
+
+    # extracting response text 
+    resp = r.text
+    status = r.status_code 
+    print(payload)
+    print(resp, status) 
+    time.sleep(5)
