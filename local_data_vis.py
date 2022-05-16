711bde31e356508c619107dfa5b32ecdd6c37d21
nicoleal88 <nicoleal88@gmail.com>
2022-05-16 12:42:19 -0300

    Fix

--- local_data_vis.py
+++ local_data_vis.py
@@ -11,14 +11,14 @@ window.title("Data Vis Offline")
 
 def clock():
     date_time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S/%p")
-    date, time1 = date_time.split()
-    time2, time3 = time1.split('/')
-    hour, minutes, seconds = time2.split(':')
-    if int(hour) > 11 and int(hour) < 24:
-        time = str(int(hour) - 12) + ':' + minutes + \
-            ':' + seconds + ' ' + time3
-    else:
-        time = time2 + ' ' + time3
+    date, time = date_time.split()
+    # time2, time3 = time1.split('/')
+    # hour, minutes, seconds = time2.split(':')
+    # if int(hour) > 11 and int(hour) < 24:
+    #     time = str(int(hour) - 12) + ':' + minutes + \
+    #         ':' + seconds + ' ' + time3
+    # else:
+    #     time = time2 + ' ' + time3
     time_label.config(text=time)
     date_label.config(text=date)
     time_label.after(1000, clock)
@@ -36,9 +36,9 @@ def update():
         f1, f2, f3 = data.split()
         f.close()  # to change file access modes
 
-    f1_label.config(text="Temp. de Ferm 1: {} °C".format(f1))
-    f2_label.config(text="Temp. de Ferm 2: {} °C".format(f2))
-    f3_label.config(text="Temp. de Ferm 3: {} °C".format(f3))
+    f1_label.config(text="Temp. de Ferm 1: {:.1f} °C".format(float(f1)))
+    f2_label.config(text="Temp. de Ferm 2: {:.1f} °C".format(float(f2)))
+    f3_label.config(text="Temp. de Ferm 3: {:.1f} °C".format(float(f3)))
     f1_label.after(1000, update)
 
 
